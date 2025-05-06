36c1453ccc168dcc4c51b7785f47d2e9a0c91d0c
root <root@usmtd-dmz-nginx.clemsworld.com>
2025-05-06 00:45:14 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Get the IP address of the client
+    if request.headers.getlist("X-Forwarded-For"):
+        client_ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        client_ip = request.remote_addr
+    
+    # Get the port of the client
+    client_port = request.environ.get('REMOTE_PORT', 'Unknown')
+    
+    # Get the browser type
+    user_agent = request.headers.get('User-Agent', 'Unknown')
+    
+    # Render the template with the gathered information
+    return render_template('index.html', 
+                          client_ip=client_ip,
+                          client_port=client_port,
+                          user_agent=user_agent)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
